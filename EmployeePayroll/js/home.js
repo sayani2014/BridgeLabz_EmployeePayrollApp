21fa98af0ae472c443d0cf3d44f682894b345d61
Sayani Koley <sayanikoley8@gmail.com>
2021-07-30 10:25:42 +0530

    Ability to view Employee Payroll details in a Tabular Format from JSON Object.

--- EmployeePayroll/js/home.js
+++ EmployeePayroll/js/home.js
@@ -7,19 +7,21 @@ const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th>" +
                     "<th>Department</th><th>Salary</th><th>Start Date</th>" +
                     "<th>Actions</th>";
-    const innerHtml = `${headerHtml}
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();  
+    for ( const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
         <tr>
             <td>
-                <img class="profile" alt="" src="../assets/profile-images/Ellipse -1.png">
+                <img class="profile" alt="" src="${empPayrollData._profilePic}">
             </td>
-            <td>Sayani Koley</td>
-            <td>Female</td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
             <td>
-                <div class="dept-label">HR</div>
-                <div class="dept-label">Engineer</div>
+                ${getDeptHtml(empPayrollData._department)}
             </td>
-            <td>300000</td>
-            <td>1 Nov 2020</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._start_date}</td>
             <td>
                 <img id="1" onclick="remove(this)" alt="delete"
                             src="../assets/icons/delete-black-18dp.svg">
@@ -27,6 +29,43 @@ const createInnerHtml = () => {
                             src="../assets/icons/create-black-18dp.svg">           
             </td>
         </tr>
-    `;
+        `;
+    }
     document.querySelector("#display").innerHTML = innerHtml;
 };
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for ( const dept of deptList ) {
+        deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Sayani Koley',
+            _gender: 'Female',
+            _department: [
+                'Engineering', 'HR'
+            ],
+            _salary: '250000',
+            _start_date: '01 Nov 2020',
+            _note: '',
+            _profilePic: '../assets/profile-images/Ellipse 1.png'
+        },
+        {
+            _name: 'Suvadeep Das',
+            _gender: 'Male',
+            _department: [
+                'Engineering', 'Finance'
+            ],
+            _salary: '500000',
+            _start_date: '29 Oct 2019',
+            _note: '',
+            _profilePic: '../assets/profile-images/Ellipse -3.png'
+        }
+    ]
+    return empPayrollListLocal;
+}
